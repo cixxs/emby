66aa33568ad70a1b61bc6f014914ae517b124997
cixxs <80149990+cixxs@users.noreply.github.com>
2021-05-30 20:38:06 +0800

    Create bbr.sh

---
+++ bbr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo root:Oracle123654 |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf && sysctl -p
